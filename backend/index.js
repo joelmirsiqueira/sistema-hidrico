61ed7c8629f07b7dc68e34055bb8108a695367a6
JoelmirSiqueira <joelmirgth10@gmail.com>
2025-12-24 13:05:44 -0300

    feat: atualizar index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
+import dotenv from 'dotenv';
 import express from 'express';
-import { router } from './routers.js';
+import router from './routers/routers.js';
+import mqttConnectAsync from './mqtt/connect.mqtt.js';
+import mongooseConnectAsync from './database/connect.database.js';
+
+dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
+
+const port = process.env.PORT;
 
 const app = express();
 
-const port = 3000;
-
 app.use(express.json());
+app.use(router)
 
-app.use(router);
+await mongooseConnectAsync();
+// await mqttConnectAsync();
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
