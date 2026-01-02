898a7f4de410d345cb51064cab609aced99bcb0e
JoelmirSiqueira <joelmirgth10@gmail.com>
2026-01-02 14:31:17 -0300

    feat: atualiza para inscrever em topicos mqtt ao conectar ao MQTT

--- backend/mqtt/connect.mqtt.js
+++ backend/mqtt/connect.mqtt.js
@@ -1,40 +1,36 @@
 import { connectAsync } from "mqtt";
 
-export default async function mqttConnectAsync() {
+async function mqttConnectAsync() {
     const brokerUrl = process.env.MQTT_URL;
+    if (!brokerUrl) {
+        throw new Error('URL do broker MQTT não especificada');
+    }
     try {
         const client = await connectAsync(brokerUrl);
         console.log('Conectado ao broker MQTT');
+        await mqttSubscribeAsync(client);
+        client.on('message', (topic, message) => {
+            console.log(`Mensagem recebida no tópico ${topic}: ${message.toString()}`);
+        });
         return client;
     } catch (error) {
         console.error('Erro ao conectar ao broker MQTT:', error);
+        throw error;
     }
 }
 
 async function mqttSubscribeAsync(client) {
-    const topicosLista = [
-        process.env.MQTT_TOPIC_STATUS_NIVEL,
-        process.env.MQTT_TOPIC_STATUS_COMPORTA_1,
-        process.env.MQTT_TOPIC_STATUS_COMPORTA_2,
-    ];
+    const topicSubscribe = process.env.MQTT_TOPIC_SUBSCRIBE;
+    if (!topicSubscribe) {
+        throw new Error('Tópico de subscrição não especificado');
+    }
     try {
-        await Promise.all(
-            topicosLista.map(async (topico) => {
-                await client.subscribe(topico);
-            })
-        );
-        console.log('Assinatura realizada com sucesso');
+        await client.subscribeAsync(topicSubscribe + '/#');
+        console.log('Inscrito nos tópicos MQTT');
     } catch (error) {
-        console.error('Erro ao assinar tópicos:', error);
+        console.error('Erro ao se inscrever no tópico MQTT:', error);
+        throw error;
     }
 }
 
-async function mqttMessageAsync(client) {
-    client.on('message', async (topic, message) => {
-        const messageString = message.toString();
-        console.log(`Mensagem recebida no tópico ${topic}: ${messageString}`);
-
-    });
-}
-
-
+export default mqttConnectAsync;
