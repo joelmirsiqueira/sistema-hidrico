a2a2dea3586627103c33916978da378f91e99498
jalmir <jalmirgth10@gmail.com>
2026-01-12 14:11:46 -0300

    fix: ajustes no dto de relato

--- backend/dtos/relato.dto.js
+++ backend/dtos/relato.dto.js
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 const relatoDto = z.object({
-    cliente: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
-    mensagem: z.string().min(3, "A mensagem deve ter pelo menos 20 caracteres"),
+    usuario: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
+    mensagem: z.string().min(3, "A mensagem deve ter pelo menos 15 caracteres"),
     status: z.enum(['lido', 'não lido'], "Status deve ser lido ou não lido").optional()
 });
 
