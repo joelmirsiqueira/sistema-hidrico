5f453ab63e7396ffed3bd5d48bf3cc1dd9147b17
Joelmir Siqueira <joelmirgth10@gmail.com>
2026-01-19 16:21:10 -0300

    refact: remove inclusoes desnecessárias e refatora medicao de nivel

--- firmware-esp32/main/app_main.c
+++ firmware-esp32/main/app_main.c
@@ -11,16 +11,18 @@
 #include "esp_log.h"
 #include "mqtt_client.h"
 #include "driver/gpio.h"
-#include <ultrasonic.h>
+#include <esp_timer.h>
+#include <esp32/rom/ets_sys.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
 static const char *TAG = "app_main";
 
 #define TOPICO_NIVEL "sgh/reservatorio/nivel"
-#define TOPICO_CONSUMO_CLIENTE "sgh/consumo/cliente"
+#define TOPICO_CLIENTE_CONSUMO "sgh/cliente/consumo"
 #define TOPICO_COMPORTA_STATUS "sgh/reservatorio/comporta/status"
-#define TOPICO_COMPORTA_ACIONAR "sgh/reservatorio/comporta/acionar"
+#define TOPICO_COMPORTA_ACIONAR "sgh/reservatorio/comporta/acionar/#"
+#define TOPICO_ERROR_LOG "sgh/error/log"
 
 #define SENSOR_NIVEL_TRIGGER GPIO_NUM_17
 #define SENSOR_NIVEL_ECHO GPIO_NUM_16
@@ -33,10 +35,10 @@ static const char *TAG = "app_main";
 #define CLIENTE_4 GPIO_NUM_23
 
 #define PULSOS_POR_LITRO 450.0
-#define MINIMO 21
+#define MINIMO 18
 
 static esp_mqtt_client_handle_t cliente_mqtt;
-static int altura = 1;
+static bool vazio = true;
 
 typedef struct
 {
@@ -62,9 +64,26 @@ static cliente_flow_t tabela_clientes[] = {
     {"4", CLIENTE_4, 0},
 };
 
-static bool is_vazio(void)
+typedef struct
 {
-    return altura < 1;
+    const char *texto;
+    const int valor;
+} comando_comporta_entry_t;
+
+static const comando_comporta_entry_t tabela_comandos[] = {
+    {"1", 1},
+    {"0", 0}};
+
+static void set_vazio(void)
+{
+    for (int i = 0; i < sizeof(tabela_comportas) / sizeof(tabela_comportas[0]); i++)
+    {
+        gpio_set_level(tabela_comportas[i].pin, 1);
+        char topic[128];
+        snprintf(topic, sizeof(topic), "%s/%s", TOPICO_COMPORTA_STATUS, tabela_comportas[i].id);
+        esp_mqtt_client_publish(cliente_mqtt, topic, "1", 0, 0, 0);
+    }
+    vazio = true;
 }
 
 void comportas_setup(void)
@@ -78,34 +97,82 @@ void comportas_setup(void)
     }
 }
 
-static void set_comporta(char *comporta_id, char *data)
+static void set_comporta(esp_mqtt_event_handle_t event)
 {
-    int comporta_num = -1;
+    char topic_safe[128];
+    char data_safe[32];
+
+    if (event->topic_len >= sizeof(topic_safe) || event->data_len >= sizeof(data_safe))
+    {
+        ESP_LOGE(TAG, "Tópico ou dado muito longo.");
+        esp_mqtt_client_publish(cliente_mqtt, TOPICO_ERROR_LOG, "Tópico ou dado muito longo", 0, 0, 0);
+        return;
+    }
+
+    memcpy(topic_safe, event->topic, event->topic_len);
+    topic_safe[event->topic_len] = '\0';
+
+    memcpy(data_safe, event->data, event->data_len);
+    data_safe[event->data_len] = '\0';
+
+    char *comporta_id = strrchr(topic_safe, '/');
+
+    if (comporta_id == NULL)
+    {
+        ESP_LOGE(TAG, "Tópico inválido: %s", topic_safe);
+        esp_mqtt_client_publish(cliente_mqtt, TOPICO_ERROR_LOG, "Tópico inválido", 0, 0, 0);
+        return;
+    }
+    comporta_id++;
+
+    int comporta_gpio_num = -1;
 
     for (int i = 0; i < sizeof(tabela_comportas) / sizeof(tabela_comportas[0]); i++)
     {
         if (strcmp(comporta_id, tabela_comportas[i].id) == 0)
         {
-            comporta_num = tabela_comportas[i].pin;
+            comporta_gpio_num = tabela_comportas[i].pin;
             break;
         }
     }
 
-    if (comporta_num == -1)
+    if (comporta_gpio_num == -1)
+    {
+        ESP_LOGE(TAG, "Comporta inválida: %s", comporta_id);
+        esp_mqtt_client_publish(cliente_mqtt, TOPICO_ERROR_LOG, "Comporta inválida", 0, 0, 0);
         return;
+    }
 
-    if (strncmp(data, "1", strlen(data)) == 0)
+    int comando_value = -1;
+
+    for (int i = 0; i < sizeof(tabela_comandos) / sizeof(tabela_comandos[0]); i++)
     {
-        gpio_set_level(comporta_num, 1);
+        if (strcmp(data_safe, tabela_comandos[i].texto) == 0)
+        {
+            comando_value = tabela_comandos[i].valor;
+            break;
+        }
     }
-    else if (strncmp(data, "0", strlen(data)) == 0)
+
+    if (comando_value == -1)
     {
-        gpio_set_level(comporta_num, 0);
+        ESP_LOGE(TAG, "Comando inválido: %s", data_safe);
+        esp_mqtt_client_publish(cliente_mqtt, TOPICO_ERROR_LOG, "Comando inválido", 0, 0, 0);
+        return;
     }
+
+    if (vazio && comando_value == 0)
+    {
+        ESP_LOGE(TAG, "Reservatório vazio");
+        esp_mqtt_client_publish(cliente_mqtt, TOPICO_ERROR_LOG, "Reservatório vazio", 0, 0, 0);
+        return;
+    }
+
+    gpio_set_level(comporta_gpio_num, comando_value);
 
     char topic[128];
     snprintf(topic, sizeof(topic), "%s/%s", TOPICO_COMPORTA_STATUS, comporta_id);
-    esp_mqtt_client_publish(cliente_mqtt, topic, data, 0, 0, 0);
+    esp_mqtt_client_publish(cliente_mqtt, topic, comando_value ? "1" : "0", 0, 0, 0);
 }
 
 static void log_error_if_nonzero(const char *message, int error_code)
@@ -126,9 +193,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        char topico[128];
-        snprintf(topico, sizeof(topico), "%s/#", TOPICO_COMPORTA_ACIONAR);
-        msg_id = esp_mqtt_client_subscribe(client, topico, 0);
+        msg_id = esp_mqtt_client_subscribe(client, TOPICO_COMPORTA_ACIONAR, 0);
         ESP_LOGI(TAG, "inscrito com sucesso, msg_id=%d", msg_id);
         break;
 
@@ -139,38 +204,10 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     case MQTT_EVENT_PUBLISHED:
         ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
         break;
+
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-
-        char topic_safe[128];
-        char data_safe[32];
-
-        if (event->topic_len >= sizeof(topic_safe) || event->data_len >= sizeof(data_safe))
-        {
-            ESP_LOGE(TAG, "Topic or data too long");
-            break;
-        }
-
-        memcpy(topic_safe, event->topic, event->topic_len);
-        topic_safe[event->topic_len] = '\0';
-
-        memcpy(data_safe, event->data, event->data_len);
-        data_safe[event->data_len] = '\0';
-
-        if (is_vazio() && strcmp(data_safe, "0") == 0)
-        {
-            strcpy(data_safe, "1");
-        }
-
-        char *comporta = strrchr(topic_safe, '/');
-
-        if (comporta == NULL)
-        {
-            ESP_LOGE(TAG, "Invalid topic: %s", topic_safe);
-            break;
-        }
-        comporta++;
-        set_comporta(comporta, data_safe);
+        set_comporta(event);
         break;
 
     case MQTT_EVENT_ERROR:
@@ -183,6 +220,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
             ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
         }
         break;
+
     default:
         ESP_LOGI(TAG, "Other event id:%d", event->event_id);
         break;
@@ -191,58 +229,85 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 
 static void ultrasonic_task(void *pvParameters)
 {
-    ultrasonic_sensor_t sensor = {
-        .trigger_pin = SENSOR_NIVEL_TRIGGER,
-        .echo_pin = SENSOR_NIVEL_ECHO};
+    gpio_reset_pin(SENSOR_NIVEL_TRIGGER);
+    gpio_set_direction(SENSOR_NIVEL_TRIGGER, GPIO_MODE_OUTPUT);
+    gpio_set_level(SENSOR_NIVEL_TRIGGER, 0);
 
-    ultrasonic_init(&sensor);
-    uint32_t distancia;
-
-    int altura_anterior = 1;
+    gpio_reset_pin(SENSOR_NIVEL_ECHO);
+    gpio_set_direction(SENSOR_NIVEL_ECHO, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(SENSOR_NIVEL_ECHO, GPIO_PULLUP_ONLY);
 
     while (true)
     {
-        esp_err_t res = ultrasonic_measure_cm(&sensor, 500, &distancia);
-        if (res != ESP_OK)
+        int64_t soma = 0;
+        for (int i = 0; i < 10; i++)
         {
-            ESP_LOGE("ultrasonic", "Error %d: ", res);
-            switch (res)
+            gpio_set_level(SENSOR_NIVEL_TRIGGER, 1);
+            ets_delay_us(10);
+            gpio_set_level(SENSOR_NIVEL_TRIGGER, 0);
+
+            bool timeout = false;
+            int64_t wait_start = esp_timer_get_time();
+
+            while (!gpio_get_level(SENSOR_NIVEL_ECHO))
             {
-            case ESP_ERR_ULTRASONIC_PING:
-                ESP_LOGE("ultrasonic", "Cannot ping (device is in invalid state)\n");
-                break;
-            case ESP_ERR_ULTRASONIC_PING_TIMEOUT:
-                ESP_LOGE("ultrasonic", "Ping timeout (no device found)\n");
-                break;
-            case ESP_ERR_ULTRASONIC_ECHO_TIMEOUT:
-                ESP_LOGE("ultrasonic", "Echo timeout (i.e. distance too big)\n");
-                break;
-            default:
-                ESP_LOGE("ultrasonic", "%s\n", esp_err_to_name(res));
-            }
-        }
-        else
-        {
-            char altura_str[16];
-            altura = MINIMO - distancia;
-            snprintf(altura_str, sizeof(altura_str), "%d", altura);
-            esp_mqtt_client_publish(cliente_mqtt, TOPICO_NIVEL, altura_str, 0, 0, 0);
-            if (altura != altura_anterior)
-            {
-                altura_anterior = altura;
-                if (is_vazio())
+                if (esp_timer_get_time() - wait_start >= 5000)
                 {
-                    for (int i = 0; i < sizeof(tabela_comportas) / sizeof(tabela_comportas[0]); i++)
-                    {
-                        gpio_set_level(tabela_comportas[i].pin, 1);
-                        char topic[128];
-                        snprintf(topic, sizeof(topic), "%s/%s", TOPICO_COMPORTA_STATUS, tabela_comportas[i].id);
-                        esp_mqtt_client_publish(cliente_mqtt, topic, "1", 0, 0, 0);
-                    }
+                    ESP_LOGW("ultrasonic", "Timeout aguardando inicio do echo");
+                    timeout = true;
+                    break;
                 }
             }
-            vTaskDelay(pdMS_TO_TICKS(2000));
+
+            if (timeout)
+            {
+                vTaskDelay(pdMS_TO_TICKS(1000));
+                continue;
+            }
+
+            int64_t echo_start = esp_timer_get_time();
+
+            while (gpio_get_level(SENSOR_NIVEL_ECHO))
+            {
+                if (esp_timer_get_time() - echo_start >= 2000)
+                {
+                    ESP_LOGW("ultrasonic", "Timeout aguardando fim do echo");
+                    timeout = true;
+                    break;
+                }
+            }
+
+            if (timeout)
+            {
+                vTaskDelay(pdMS_TO_TICKS(1000));
+                continue;
+            }
+
+            int64_t tempo_echo = esp_timer_get_time() - echo_start;
+            soma += tempo_echo;
+            if (!vazio && tempo_echo > 1085)
+                set_vazio();
+
+            vTaskDelay(pdMS_TO_TICKS(200));
         }
+
+        int64_t tempo_echo = soma / 10;
+        ESP_LOGI("ultrasonic", "%" PRId64, tempo_echo);
+
+        int distancia_cm = (tempo_echo * 0.0343) / 2;
+        ESP_LOGI("ultrasonic", "%d", distancia_cm);
+
+        int altura = MINIMO - distancia_cm;
+        ESP_LOGI("ultrasonic", "%d", altura);
+        altura = altura < 0 ? 0 : altura;
+        if (altura > 0 && vazio)
+            vazio = false;
+
+        char altura_str[16];
+        snprintf(altura_str, sizeof(altura_str), "%d", altura);
+        esp_mqtt_client_publish(cliente_mqtt, TOPICO_NIVEL, altura_str, 0, 0, 0);
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
@@ -288,15 +353,18 @@ static void fluxometro_task(void *pvParameters)
                 else
                 {
                     float litros = (float)pulsos_atuais / PULSOS_POR_LITRO;
-                    char topic[128];
-                    char payload[16];
-                    snprintf(topic, sizeof(topic), "%s/%s", TOPICO_CONSUMO_CLIENTE, tabela_clientes[i].id);
-                    snprintf(payload, sizeof(payload), "%.3f", litros);
-
-                    esp_mqtt_client_publish(cliente_mqtt, topic, payload, 0, 0, 0);
-                    ESP_LOGI(TAG, "Cliente %s: %.3f Litros", tabela_clientes[i].id, litros);
-
-                    tabela_clientes[i].pulsos = 0;
+                    if (litros > 0.070)
+                    {
+                        ESP_LOGI(TAG, "Cliente %s: %.3f Litros", tabela_clientes[i].id, litros);
+                        
+                        char topic[128];
+                        snprintf(topic, sizeof(topic), "%s/%s", TOPICO_CLIENTE_CONSUMO, tabela_clientes[i].id);
+                        char payload[16];
+                        snprintf(payload, sizeof(payload), "%.1f", litros);
+                        esp_mqtt_client_publish(cliente_mqtt, topic, payload, 0, 0, 0);
+                        
+                        tabela_clientes[i].pulsos = 0;
+                    }
                 }
             }
         }
