f2b265d5f089af17492cc6ec05fd20d70458213c
Jalmir Siqueira <jalmirgth10@gmail.com>
2026-01-19 18:08:32 -0300

    refactor: mudança na abordagem da criação de endereços

--- backend/dtos/endereco.dto.js
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-const enderecoDto = z.object({
-    rua: z.string().min(3, "O nome deve ter pelo menos 5 caracteres"),
-    numero: z.number(),
-    bairro: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
-});
-
-export default enderecoDto;
