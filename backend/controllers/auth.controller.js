e635e74a23d8ffdd8326df421ceb18161b47893e
Joelmir Siqueira <joelmirgth10@gmail.com>
2026-01-25 18:50:19 -0300

    refactor: ajustes no controller de autenticação

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,40 +1,34 @@
-// backend/controllers/auth.controller.js
-import Usuario from "../models/usuario.model.js";
+import { respostaUsuarioDto } from "../dtos/usuario.dto.js";
+import { Usuario } from "../models/usuario.model.js";
 import jwt from "jsonwebtoken";
 
 const login = async function(req, res) {
     try {
         const { email, senha } = req.body;
 
-        // 1. Verificar se o usuário existe
-        // Precisamos usar .select('+senha') porque definimos select: false no model
-        const usuario = await Usuario.findOne({ email }).select('+senha');
+        const consulta = await Usuario.findOne({ email }).select('+senha');
 
-        if (!usuario) {
+        if (!consulta) {
             return res.status(401).json({ message: "Usuário ou senha inválidos." });
         }
 
-        // 2. Verificar se a senha está correta
-        const senhaValida = await usuario.compararSenha(senha);
+        const senhaValida = await consulta.compararSenha(senha);
 
         if (!senhaValida) {
             return res.status(401).json({ message: "Usuário ou senha inválidos." });
         }
 
-        // 3. Gerar o Token JWT
-        // O payload deve conter informações úteis, mas não sensíveis
         const payload = {
-            id: usuario._id,
-            nome: usuario.nome,
-            tipo: usuario.tipo,
+            id: consulta._id,
+            nome: consulta.nome,
+            tipo: consulta.tipo,
         };
 
         const token = jwt.sign(payload, process.env.JWT_SECRET, {
             expiresIn: parseInt(process.env.JWT_EXPIRES_IN)
         });
 
-        // 4. Retornar dados (sem a senha)
-        usuario.senha = undefined;
+        const usuario = respostaUsuarioDto.parse(consulta);
 
         res.status(200).json({
             message: "Login realizado com sucesso!",
