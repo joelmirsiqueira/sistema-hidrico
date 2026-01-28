b1d6beb582552042b5f5a9e2254b3c718a814a30
Daniel Ferreira <pocketrocket@outlook.com.br>
2026-01-28 12:15:47 -0300

    feat: nível do reservatório sincronizado com o back

--- frontend/src/components/dashboard/ReservoirLevelCard.jsx
+++ frontend/src/components/dashboard/ReservoirLevelCard.jsx
@@ -1,12 +1,42 @@
 import BaseCard from "./BaseCard"
 import { Typography } from "@mui/joy"
+import { useEffect, useState } from "react";
 
 export default function ReservoirLevelCard() {
+    const [nivel, setNivel] = useState(null);
+    const [erro, setErro] = useState("");
+
+    useEffect(() => {
+        async function buscarNivel() {
+            try {
+                const token = localStorage.getItem("token");
+
+                const response = await fetch(
+                    "http://localhost:3000/funcionario/consultar/nivel", {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+
+                const data = await response.json();
+                if (!response.ok) {
+                    throw new Error(data.error || "Erro ao obter nível do reservatório");
+                }
+
+                setNivel(data.capacidade);
+            } catch (error) {
+                setErro(error.message);
+            }
+        }
+
+        buscarNivel();
+    }, []);
+
     return (
         <BaseCard sx={{ bgcolor: "#ffc934", color: "#fff" }}>
             <Typography level="body-sm">Nível do Reservatório</Typography>
-            <Typography level="h2">253.593L ou 200m³</Typography>
-            <Typography level="body-xs">Capacidade total: 500m³</Typography>
+            <Typography level="h2">{nivel*1000}L ou {nivel}m³</Typography>
         </BaseCard>
     )
 }
