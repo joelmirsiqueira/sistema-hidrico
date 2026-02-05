aa7347a0e68a2b80c7ad5eb30548435fa594d92f
Daniel Ferreira <pocketrocket@outlook.com.br>
2026-02-05 18:30:05 -0300

    feat: card report problem integrado ao back

--- frontend/src/components/dashboard/ReportProblemCard.jsx
+++ frontend/src/components/dashboard/ReportProblemCard.jsx
@@ -1,28 +1,69 @@
-import BaseCard from "./BaseCard"
-import { Box, Typography, Button } from "@mui/joy"
+import BaseCard from "./BaseCard";
+import { Box, Typography, Button } from "@mui/joy";
+import { useState } from "react";
+import ModalReportProblem from "./ModalReportProblem";
+import { jwtDecode } from "jwt-decode";
 
 export default function ReportProblemCard() {
+    const [open, setOpen] = useState(false);
+    const [erro, setErro] = useState("");
+
+    async function criarRelato(mensagem) {
+        try {
+            const token = localStorage.getItem("token");
+
+            const response = await fetch("http://localhost:3000/cliente/criar/relato", {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ mensagem }),
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.error || "Erro ao realizar login");
+            }
+        } catch (error) {
+            setErro(error.message);
+        }
+    }
+
+
     return (
-        <BaseCard sx={{
-            width: "35%",
-            padding: "30px",
-            background: "linear-gradient(90deg, #14223D, #17171A)",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center"
-        }}
-        >
-            <Box sx={{
-                height: "100%",
-                display: "flex",
-                flexDirection: "column",
+        <>
+            <BaseCard sx={{
+                width: "35%",
+                padding: "30px",
+                background: "linear-gradient(90deg, #14223D, #17171A)",
+                flexDirection: "row",
+                justifyContent: "center",
                 alignItems: "center",
-                gap: "20px"
-            }}>
-                <Typography textColor="#fff" level="h2">VAZAMENTOS OU PROBLEMAS NO ABASTECIMENTO?</Typography>
-                <Button color="danger" sx={{width: "50%"}}>RELATE AQUI</Button>
-            </Box>
-        </BaseCard>
-    )
+                textAlign: "center"
+            }}
+            >
+                <Box sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    gap: "20px"
+                }}>
+                    <Typography textColor="#fff" level="h2">VAZAMENTOS OU PROBLEMAS NO ABASTECIMENTO?</Typography>
+                    <Button color="danger" onClick={() => setOpen(true)} sx={{ width: "50%" }}>RELATE AQUI</Button>
+                </Box>
+            </BaseCard>
+
+            <ModalReportProblem
+                open={open}
+                onClose={() => setOpen(false)}
+                onSubmit={(mensagem) => {
+                    criarRelato(mensagem);
+                    setOpen(false);
+                }}
+            />
+        </>
+    );
 }
